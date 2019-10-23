54590f29fbb4929f73caf3714c6870123e044189
Keith Cheveralls <keith.chev@gmail.com>
2019-10-23 13:21:12 -0700

    new method for interpolating focusdrive positions this is intended to be a short-term fix for nonplanar plates - it interpolates from an arbitrary list of measured positions, rather than just the corners of the region of wells to be imaged.

--- dragonfly_automation/utils.py
+++ dragonfly_automation/utils.py
@@ -32,17 +32,13 @@ def to_uint8(im):
     return im
 
 
-def interpolate_stage_positions(
+def interpolate_focusdrive_positions_from_corners(
     position_list_filepath, 
     region_shape, 
     num_positions_per_well,
     corner_positions):
     '''
     This method refactors the 'StageTiltDragonfly.py' script
-
-    Assumptions
-    -----------
-
 
     Parameters
     ----------
@@ -54,8 +50,8 @@ def interpolate_stage_positions(
     num_positions_per_well : int
         The number of positions per well
     corner_positions: tuple of tuples
-        The user-measured z-positions of the corners of the plate region,
-        as a tuple of the form
+        The user-measured z-positions (FocusDrive device positions) 
+        of the corners of the plate region, as a tuple of the form
         (
             (top_left, top_right), 
             (bottom_left, bottom_right),
@@ -113,6 +109,89 @@ def interpolate_stage_positions(
     # save the new position_list
     ext = position_list_filepath.split('.')[-1]
     new_filepath = re.sub('.%s$' % ext, '_INTERPOLATED.%s' % ext, position_list_filepath)
+    with open(new_filepath, 'w') as file:
+        json.dump(position_list, file)
+
+    return new_filepath, position_list
+
+
+
+def well_id_to_position(well_id):
+    '''
+    'A1' to (0, 0), 'H12' to (7, 11), etc
+    '''
+    pattern = r'^([A-H])([0-9]{1,2})$'
+    result = re.findall(pattern, well_id)
+    row, col = result[0]
+    row_ind = list('ABCDEFGH').index(row)
+    col_ind = int(col) - 1
+    return row_ind, col_ind
+
+
+def parse_hcs_site_label(label):
+    '''
+    Parse an HCS site label
+    ** copied from PipelinePlateProgram **
+    '''
+    pattern = r'^([A-H][0-9]{1,2})-Site_([0-9]+)$'
+    result = re.findall(pattern, label)
+    well_id, site_num = result[0]
+    site_num = int(site_num)
+    return well_id, site_num
+
+
+def interpolate_focusdrive_positions_from_all(
+    position_list_filepath, 
+    measured_focusdrive_positions, 
+    top_left_well_id,
+    bottom_right_well_id):
+    '''
+
+    measured_focusdrive_positions = {
+        'B9': 7600,
+        'B5': 7500,
+        'B2': 7600,
+        ...
+    }
+
+    '''
+
+    # create an array of numeric (x,y,z) positions from the well_ids
+    measured_positions = np.array([
+        (*well_id_to_position(well_id), pos) 
+            for well_id, pos in measured_focusdrive_positions.items()])
+
+    interpolator = interpolate.interp2d(
+        measured_positions[:, 0], 
+        measured_positions[:, 1], 
+        measured_positions[:, 2], 
+        kind='linear')
+
+    with open(position_list_filepath) as file:
+        position_list = json.load(file)
+
+    for ind, pos in enumerate(position_list['POSITIONS']):
+        
+        well_id, site_num = parse_hcs_site_label(pos['LABEL'])
+        x, y = well_id_to_position(well_id)
+
+        # the interpolated z-position of the current well
+        interpolated_position = interpolator(x, y)[0]
+
+        # the config entry for the 'FocusDrive' device (this is the motorized z-stage)
+        focus_drive_config = {
+            'X': interpolated_position,
+            'Y': 0,
+            'Z': 0,
+            'AXES': 1,
+            'DEVICE': 'FocusDrive',
+        }
+
+        position_list['POSITIONS'][ind]['DEVICES'].append(focus_drive_config)
+    
+    # save the new position_list
+    ext = position_list_filepath.split('.')[-1]
+    new_filepath = re.sub('.%s$' % ext, '_INTERPOLATED-FROM-ALL.%s' % ext, position_list_filepath)
     with open(new_filepath, 'w') as file:
         json.dump(position_list, file)
 
