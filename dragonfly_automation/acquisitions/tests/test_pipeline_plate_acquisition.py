a802ac505d17571f87e4ce633de568988199a843
Keith Cheveralls <keith.chev@gmail.com>
2021-09-07 13:41:00 -0700

    initial test for the acquisition script itself

---
+++ dragonfly_automation/acquisitions/tests/test_pipeline_plate_acquisition.py
@@ -0,0 +1,61 @@
+
+import os
+import json
+import pathlib
+import pandas as pd
+
+from dragonfly_automation.acquisitions.pipeline_plate_acquisition import PipelinePlateAcquisition
+from dragonfly_automation.acquisitions.pipeline_plate_settings import fov_selection_settings
+
+def test_acquisition_test_mode(tmpdir, trained_fov_scorer):
+
+    pml_id = 'PML0123'
+    plate_id = 'P0001'
+    platemap_type = 'none'
+
+    acquisition = PipelinePlateAcquisition(
+        root_dir=str(tmpdir), 
+        pml_id=pml_id,
+        plate_id=plate_id,
+        platemap_type=platemap_type,
+        mock_micromanager_api=True,
+        mocked_mode='random-real',
+        fov_scorer=trained_fov_scorer,
+        skip_fov_scoring=False,
+        acquire_brightfield_stacks=False,
+    )
+    acquisition.setup()
+
+    # run the acquisition in test mode (only visit one well and take one z-stack)
+    acquisition.run(mode='test', test_mode_well_id=None)
+
+    with open(tmpdir / 'metadata.json', 'r') as file:
+        metadata = json.load(file)
+
+    assert metadata.get('pml_id') == pml_id
+    assert metadata.get('plate_id') == plate_id
+    assert metadata.get('platemap_type') == platemap_type
+
+    # check that the acquisition script exited without crashing
+    with open(tmpdir / 'logs' / 'experiment-metadata.json', 'r') as file:
+        metadata = json.load(file)
+    assert 'cleanup_timestamp' in metadata.keys()
+
+    # check that all of the logfiles were generated
+    assert os.path.isfile(tmpdir / 'logs' / 'afc-calls.csv')
+    assert os.path.isfile(tmpdir / 'logs' / 'all-events.log')
+    assert os.path.isfile(tmpdir / 'logs' / 'important-events.log')
+
+    # there should have been no errors
+    assert not os.path.isfile(tmpdir / 'logs' / 'error-events.json')
+
+    # get the number of good FOVs found (this varies because mocked FOVs are selected randomly)
+    # TODO: what to do with this?
+    fovs = pd.read_csv(tmpdir / 'logs' / 'fov-scoring' / 'fov-score-log.csv')
+    num_good_fovs = (fovs.score > fov_selection_settings.min_score).sum()
+
+    # check that the expected number of z-stacks were acquired 
+    # (in test mode this is only two, for 405 and 488 channels at one FOV)
+    df = pd.read_csv(tmpdir / 'logs' / 'acquired-images.csv')
+    assert df.shape[0] ==  2
+    
--- dragonfly_automation/conftest.py
+++ dragonfly_automation/conftest.py
@@ -8,11 +8,13 @@ from dragonfly_automation.fov_models import PipelineFOVScorer
 
 @pytest.fixture(scope='session')
 def project_dir():
-    return pathlib.Path(__file__).parent.parent.parent
+    return pathlib.Path(__file__).parent.parent
+
 
 @pytest.fixture(scope='session')
 def snaps_dir(project_dir):
     return project_dir / 'dragonfly_automation' / 'tests' / 'artifacts' / 'snaps'
+
 
 @pytest.fixture(scope='session')
 def fov_snaps_high_score(snaps_dir):
@@ -21,6 +23,7 @@ def fov_snaps_high_score(snaps_dir):
         imageio.imread(snaps_dir / 'good-2.tif'),
         imageio.imread(snaps_dir / 'good-3.tif'),
     ]
+
 
 @pytest.fixture(scope='session')
 def fov_snaps_low_score(snaps_dir):
@@ -51,11 +54,9 @@ def fov_snap_no_nuclei(snaps_dir):
 @pytest.fixture(scope='session')
 def trained_fov_scorer(project_dir):
 
-    fov_scorer = PipelineFOVScorer(
-        save_dir=(project_dir / 'models' / '2019-10-08'), mode='prediction', random_state=42
-    )
+    fov_model_dir = str(project_dir / 'models' / '2019-10-08')
+    fov_scorer = PipelineFOVScorer(save_dir=fov_model_dir, mode='prediction', random_state=42)
     fov_scorer.load()
     fov_scorer.train()
     fov_scorer.validate()
     return fov_scorer
-
